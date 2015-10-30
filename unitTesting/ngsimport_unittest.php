88832c93ada123452da13701c3b4f7130c9320d0
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-30 15:44:21 -0400

    model test

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -3,7 +3,7 @@
 class ngsimport_unittest extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		include 'config/config.php';
+		include 'library/vanillamodel.class.php';
 	    include 'application/models/ngsimport.php';
 		
 		$this->assertEquals(1+1,2);

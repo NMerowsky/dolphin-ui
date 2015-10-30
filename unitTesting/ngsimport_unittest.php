8ddb2402c44142bdb9e3e85edb845017a3e82dc5
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-30 16:03:07 -0400

    first unit test

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -1,14 +1,14 @@
 <?php
 
+include 'config/config.php';
 include 'library/sqlquery.class.php';
 include 'library/vanillamodel.class.php';
-include 'application/models/ngsimport.php';
-#include 'config/config.php';
-#include 'includes/dbfuncs.php';
 
-#$query = new dbfuncs();
+include 'application/models/ngsimport.php';
 $ngsimport = new Ngsimport();
 
+#include 'includes/dbfuncs.php';
+#$query = new dbfuncs();
 
 class ngsimport_unittest extends PHPUnit_Framework_TestCase
 {

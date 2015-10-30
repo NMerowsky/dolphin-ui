f72fac13ca6a1444f6376a6edac368724f12e0a5
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-30 15:58:03 -0400

    first unit test

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -3,17 +3,17 @@
 include 'library/sqlquery.class.php';
 include 'library/vanillamodel.class.php';
 include 'application/models/ngsimport.php';
-include 'config/config.php';
-include 'includes/dbfuncs.php';
+#include 'config/config.php';
+#include 'includes/dbfuncs.php';
 
-$query = new dbfuncs();
+#$query = new dbfuncs();
 $ngsimport = new Ngsimport();
 
 
 class ngsimport_unittest extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(1+1,2);
+	public function testNum2Aplha() {
+		$this->assertEquals($ngsimport->num2alpha(3),'C');
   	}
 }
 

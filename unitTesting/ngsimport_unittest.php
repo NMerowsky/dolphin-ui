3a475ebe94b68989ec46e75520ce666b5c23d2db
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-30 16:39:05 -0400

    inflection

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -1,12 +1,12 @@
 <?php
 
 include 'config/config.php';
-include 'config/inflection.class.php';
+include 'library/inflection.class.php';
 include 'library/sqlquery.class.php';
 include 'library/vanillamodel.class.php';
 
 include 'application/models/ngsimport.php';
-
+$ngsimport = new Ngsimport();
 
 #include 'includes/dbfuncs.php';
 #$query = new dbfuncs();
@@ -15,7 +15,6 @@ class ngsimport_unittest extends PHPUnit_Framework_TestCase
 {
 	public function testNum2Aplha() {
 		echo 'testNum2Alpha';
-		$ngsimport = new Ngsimport();
 		$this->assertEquals($ngsimport->num2alpha(3),'D');
   	}
 }

09255df0008eee75d5c529f632818fdb1ae0855b
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-30 16:42:06 -0400

    inflection

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -3,6 +3,7 @@
 include 'config/config.php';
 include 'library/inflection.class.php';
 include 'library/sqlquery.class.php';
+include 'library/model.class.php';
 include 'library/vanillamodel.class.php';
 
 include 'application/models/ngsimport.php';

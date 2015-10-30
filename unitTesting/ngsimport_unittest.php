ec47cdae5d5b992b9d78b867008afc55bf36d828
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-30 16:45:42 -0400

    inflection

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -1,10 +1,8 @@
 <?php
 
 include 'config/config.php';
-include 'library/*';
-#include 'library/sqlquery.class.php';
-#include 'library/model.class.php';
-#include 'library/vanillamodel.class.php';
+include 'library/sqlquery.class.php';
+include 'library/vanillamodel.class.php';
 
 include 'application/models/ngsimport.php';
 $ngsimport = new Ngsimport();

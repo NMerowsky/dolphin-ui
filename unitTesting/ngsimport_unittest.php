47b8fb844d455f9de88c389b67eddca67d7d735b
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-30 14:28:41 -0400

    test push

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -2,8 +2,8 @@
 
 class ngsimport_unittest extends PHPUnit_Framework_TestCase
 {
-	require_once("../config/config.php");
-	require_once("../includes/dbfuncs.php");
+	include("../config/config.php");
+	include("../includes/dbfuncs.php");
 
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);

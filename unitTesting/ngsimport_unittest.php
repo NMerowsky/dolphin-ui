f844668aa126f8014e6b0c9f10234f4c5668b78e
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-31 17:38:54 -0400

    apache

--- unitTesting/ngsimport_unittest.php
+++ unitTesting/ngsimport_unittest.php
@@ -10,15 +10,12 @@ $_SESSION['uid'] = 1;
 $_SESSION['gids'] = 1;
 $_SESSION['user'] = 'kucukura';
 
-require_once 'application/models/ngsimport.php';
-$ngs = new Ngsimport();
+include 'application/models/ngsimport.php';
 
 class ngsimport_unittest extends PHPUnit_Framework_TestCase
 {	
 	public function testNum2Aplha() {
-		echo $ngs;
 		$ngsimport = new Ngsimport();
-		echo $ngsimport;
 		$this->assertEquals($ngsimport->num2alpha(3),'C');
   	}
 }

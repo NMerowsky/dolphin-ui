e9a964272d7204637d8162839cede4594363df5f
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-10-31 16:42:55 -0400

    apache

--- library/vanillamodel.class.php
+++ library/vanillamodel.class.php
@@ -5,12 +5,10 @@ class VanillaModel extends SQLQuery {
 	function __construct() {
 		
 		global $inflect;
-		echo DB_HOST;
-		echo DB_USER;
-		echo DB_PASSWORD;
-		echo DB_NAME;
+
 		$this->connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 		$this->_model = get_class($this);
+		echo $this->_model;
 		$this->_table = strtolower($inflect->pluralize($this->_model));
 	}
 

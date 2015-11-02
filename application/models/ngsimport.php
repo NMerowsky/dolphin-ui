6edeb6452339eb8906781ed19a936a1173ba6cb5
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-11-02 18:05:41 -0500

    unittest

--- application/models/ngsimport.php
+++ application/models/ngsimport.php
@@ -502,6 +502,7 @@ class Ngsimport extends VanillaModel {
 			$ll_id = json_decode($this->query("SELECT id FROM ngs_lanes WHERE name = '$ll' and series_id in 
 											(SELECT id FROM ngs_experiment_series WHERE experiment_name = '".$this->experiment_name."')"));
 			var_dump($ll_id);
+			
 			array_push($this->lane_ids, $ll_id[0]->id);
 		}
 		return $text;

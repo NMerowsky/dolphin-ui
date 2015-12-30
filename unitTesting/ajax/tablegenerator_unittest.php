981f621a1a276a504bf2518631779adfe6c6353c
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-12-30 13:40:34 -0500

    testing includes

--- unitTesting/ajax/tablegenerator_unittest.php
+++ unitTesting/ajax/tablegenerator_unittest.php
@@ -3,39 +3,38 @@
 if (!isset($_SESSION) || !is_array($_SESSION)) session_start();
 chdir('public/ajax/');
 
-echo 'test';
-
 class tablegenerator_unittest extends PHPUnit_Framework_TestCase
 {
 	public function testGetTableSamples() {
+		$this->assertEquals(1,1);
+		$_GET['p'] = 'getTableSamples';
+		$_GET['search'] = 1;
 		include 'tablegenerator.php';
-		echo $_GET['p'];
-		echo $data;
-		$this->assertEquals($_GET['search'],1);
+		$this->assertEquals(1,1);
 	}
 	
 	public function testGetTableRuns() {
+		$this->assertEquals(1,1);
 		$_GET['p'] = 'getTableRuns';
 		$_GET['search'] = 1;
-		$data = include 'tablegenerator.php';
-		echo $_GET['p'];
-		echo $data;
+		include 'tablegenerator.php';
+		$this->assertEquals(1,1);
 	}
 	
 	//find wkey example
 	public function testGetTableReportsList() {
+		$this->assertEquals(1,1);
 		$_GET['p'] = 'getTableReportsList';
 		$_GET['wkey'] = '';
-		$data = include 'tablegenerator.php';
-		echo $_GET['p'];
-		echo $data;
+		include 'tablegenerator.php';
+		$this->assertEquals(1,1);
 	}
 	
 	public function testSamplesWithRuns() {
+		$this->assertEquals(1,1);
 		$_GET['p'] = 'sampleWithRuns';
-		$data = include 'tablegenerator.php';
-		echo $_GET['p'];
-		echo $data;
+		include 'tablegenerator.php';
+		$this->assertEquals(1,1);
 	}
 }
 

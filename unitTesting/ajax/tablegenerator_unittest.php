763a9b592f59047e559783e397a71eaf155a5c93
Rhaknam <nmerowsky@gmail.com>
2015-12-31 14:16:19 -0500

    creating tables

--- unitTesting/ajax/tablegenerator_unittest.php
+++ unitTesting/ajax/tablegenerator_unittest.php
@@ -1,6 +1,7 @@
 <?php
 //	Include files needed to test ngsimport
 if (!isset($_SESSION) || !is_array($_SESSION)) session_start();
+$_SESSION['uid'] = 1;
 chdir('public/ajax/');
 
 class tablegenerator_unittest extends PHPUnit_Framework_TestCase
@@ -13,24 +14,40 @@ class tablegenerator_unittest extends PHPUnit_Framework_TestCase
 	}
 	
 	public function testGetTableRuns() {
-		$_GET['p'] = 'getTableRuns';
-		$_GET['search'] = 1;
+		$p = 'getTableRuns';
+		$search = 1;
 		include('tablegenerator.php');
 		$this->assertEquals(json_decode($data)[0]->sample_id,1);
 	}
 	
 	//find wkey example
 	public function testGetTableReportsList() {
-		$_GET['p'] = 'getTableReportsList';
-		$_GET['wkey'] = 'J98Oe0bSZ18fBx9pPuDnsD8ITRVPGV';
+		$p = 'getTableReportsList';
+		$wkey = '3pl8cmzYJ4ezgX2a9RevZxHmihpOA';
 		include('tablegenerator.php');
 		$this->assertEquals(json_decode($data)[0]->file,'rsem/genes_expression_tpm.tsv');
 	}
 	
 	public function testSamplesWithRuns() {
-		$_GET['p'] = 'samplesWithRuns';
+		$p = 'samplesWithRuns';
 		include('tablegenerator.php');
 		$this->assertEquals(json_decode($data)[0]->sample_id,1);
+	}
+	
+	public function testCreateNewTable(){
+		$p = 'createNewTable';
+		$search = 'samples=1,2,3,4,5,6:3&file=rsem/genes_expression_tpm.tsv&common=gene,transcript&key=gene&format=json';
+		$file = 'kucukura_2015-11-05-15-07-05.json2';
+		include('tablegenerator.php');
+		$this->assertEquals(json_decode($data),'true');
+	}
+	
+	public function testGetCreatedTables(){
+		
+	}
+	
+	public function testDeleteTable(){
+		
 	}
 }
 

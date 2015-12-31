9985fda946eda329958dbfe97e26b36f8a16f5af
Rhaknam <nmerowsky@gmail.com>
2015-12-31 16:04:57 -0500

    creating tables

--- public/ajax/tablegenerator.php
+++ public/ajax/tablegenerator.php
@@ -156,7 +156,7 @@ else if ($p == 'convertToTSV')
 	if (isset($_GET['url'])){$url = $_GET['url'];}
 	$json = file_get_contents($url);
 	$json_data = json_decode($json);
-	
+	echo $json;
 	$user = $_SESSION['user'].'_'.date('Y-m-d-H-i-s').'.tsv';
 	$file = fopen('../tmp/files/'.$user, "w");
 	foreach($json_data as $jd){

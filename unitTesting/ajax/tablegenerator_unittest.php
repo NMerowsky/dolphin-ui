fd43bf604c709529c6507441f6ae67179cb220ec
Rhaknam <nmerowsky@gmail.com>
2015-12-31 15:50:23 -0500

    creating tables

--- unitTesting/ajax/tablegenerator_unittest.php
+++ unitTesting/ajax/tablegenerator_unittest.php
@@ -62,12 +62,7 @@ class tablegenerator_unittest extends PHPUnit_Framework_TestCase
 	
 	public function testConvertToTSV(){
 		$p = 'convertToTSV';
-		$url = '/home/travis/build/Rhaknam/dolphin-ui/public/api/getsamplevals.php';
-		$_GET['samples'] = 'samples=1,2,3,4,5,6:3';
-		$_GET['file'] = 'file=rsem/genes_expression_tpm.tsv';
-		$_GET['common'] = 'common=gene,transcript';
-		$_GET['key'] = 'key=gene';
-		$_GET['format'] = 'format=json';
+		$url = '/home/travis/build/Rhaknam/dolphin-ui/public/api/getsamplevals.php?samples=1,2,3,4,5,6:3&file=rsem/genes_expression_tpm.tsv&common=gene,transcript&key=gene&format=json';
 		include('tablegenerator.php');
 		$file = json_decode($data);
 		$this->assertEquals(json_decode($data),$file);

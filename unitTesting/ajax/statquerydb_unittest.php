e879832b8793fa91aa84995e4a1497335ea9b5f9
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-01-04 11:13:31 -0500

    statquery test

--- unitTesting/ajax/statquerydb_unittest.php
+++ unitTesting/ajax/statquerydb_unittest.php
@@ -13,6 +13,54 @@ class statquerydb_unittest extends PHPUnit_Framework_TestCase
 		include('statquerydb.php');
 		$this->assertEquals(1,1);
 	}
+	
+	public function testGetTopUsers(){
+		echo '@@@@@@@@@@@@@@@@@@@@@@';
+		$_GET['p'] = 'getTopUsers';
+		include('statquerydb.php');
+		$this->assertEquals(1,1);
+	}
+	
+	public function testGetTopUsersTime(){
+		echo '@@@@@@@@@@@@@@@@@@@@@@';
+		$_GET['p'] = 'getTopUsersTime';
+		$_GET['type'] = 'Dolphin';
+		include('statquerydb.php');
+		$this->assertEquals(1,1);
+	}
+	
+	public function testGetUsersTime(){
+		echo '@@@@@@@@@@@@@@@@@@@@@@';
+		$_GET['p'] = 'getUsersTime';
+		$_GET['type'] = 'Dolphin';
+		include('statquerydb.php');
+		$this->assertEquals(1,1);
+	}
+	
+	public function testGetLabsTime(){
+		echo '@@@@@@@@@@@@@@@@@@@@@@';
+		$_GET['p'] = 'getLabsTime';
+		$_GET['type'] = 'Dolphin';
+		include('statquerydb.php');
+		$this->assertEquals(1,1);
+	}
+	
+	public function testGetToolTime(){
+		echo '@@@@@@@@@@@@@@@@@@@@@@';
+		$_GET['p'] = 'getToolTime';
+		$_GET['type'] = 'Dolphin';
+		include('statquerydb.php');
+		$this->assertEquals(1,1);
+	}
+	
+	public function testGetJobTime(){
+		echo '@@@@@@@@@@@@@@@@@@@@@@';
+		$_GET['p'] = 'getJobTime';
+		include('statquerydb.php');
+		$this->assertEquals(1,1);
+	}
+	
+	
 }
 
 ?>

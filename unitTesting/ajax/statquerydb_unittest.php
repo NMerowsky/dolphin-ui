fdf1acea0cbb1c0b611eabf4df6ff7063446fa60
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-01-21 11:43:05 -0500

    output buffer test

--- unitTesting/ajax/statquerydb_unittest.php
+++ unitTesting/ajax/statquerydb_unittest.php
@@ -9,48 +9,62 @@ class statquerydb_unittest extends PHPUnit_Framework_TestCase
 {
 	public function testGetDailyRuns(){
 		$_GET['p'] = 'getDailyRuns';
+		ob_start();
 		include('statquerydb.php');
+		ob_end_clean();
 		$this->assertEquals(1,1);
 	}
 	
 	public function testGetTopUsers(){
 		$_GET['p'] = 'getTopUsers';
 		$_GET['type'] = 'Dolphin';
+		ob_start();
 		include('statquerydb.php');
+		ob_end_clean();
 		$this->assertEquals(1,1);
 	}
 	
 	public function testGetTopUsersTime(){
 		$_GET['p'] = 'getTopUsersTime';
 		$_GET['type'] = 'Dolphin';
+		ob_start();
 		include('statquerydb.php');
+		ob_end_clean();
 		$this->assertEquals(1,1);
 	}
 	
 	public function testGetUsersTime(){
 		$_GET['p'] = 'getUsersTime';
 		$_GET['type'] = 'Dolphin';
+		ob_start();
 		include('statquerydb.php');
+		ob_end_clean();
 		$this->assertEquals(1,1);
 	}
 	
 	public function testGetLabsTime(){
 		$_GET['p'] = 'getLabsTime';
 		$_GET['type'] = 'Dolphin';
+		ob_start();
 		include('statquerydb.php');
+		ob_end_clean();
 		$this->assertEquals(1,1);
 	}
 	
 	public function testGetToolTime(){
 		$_GET['p'] = 'getToolTime';
 		$_GET['type'] = 'Dolphin';
+		ob_start();
 		include('statquerydb.php');
+		ob_end_clean();
 		$this->assertEquals(1,1);
 	}
 	
 	public function testGetJobTime(){
 		$_GET['p'] = 'getJobTime';
+		ob_start();
 		include('statquerydb.php');
+		ob_end_clean();
 		$this->assertEquals(1,1);
 	}
 }

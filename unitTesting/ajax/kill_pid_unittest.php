6c430eb9c10dc5f9c3f114e8e58f956d485768e4
Rhaknam <alper.kucukural@umassmed.edu>
2016-01-24 23:38:57 -0500

    ngsalterdb

---
+++ unitTesting/ajax/kill_pid_unittest.php
@@ -0,0 +1,18 @@
+<?php
+//	Include files needed to test ngsimport
+if (!isset($_SESSION) || !is_array($_SESSION)) session_start();
+$_SESSION['uid'] = '1';
+$_SESSION['user'] = 'kucukura';
+chdir('public/ajax/');
+
+class kill_pid_unittest extends PHPUnit_Framework_TestCase
+{
+    public function testKillRun(){
+        $_GET['p'] = 'killRun';
+        $_GET['run_id'] = '1';
+        include("kill_pid.php");
+        $this->assertEquals(json_decode($data)[0]->id,'0');
+    }
+}
+
+?>

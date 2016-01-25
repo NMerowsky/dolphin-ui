91d4e6c4e7a3cfe1a7bd7d76448e9d90119e2ae2
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-01-25 16:57:18 -0500

    datafuncs

--- public/ajax/dolphinfuncs.php
+++ public/ajax/dolphinfuncs.php
@@ -20,9 +20,9 @@ if($p == "getFileList")
 }
 else if($p == "updateHashBackup")
 {
-   if (isset($_GET['input'])){$input = rawurldecode($_GET['input']);}
-   if (isset($_GET['dirname'])){$dirname = rawurldecode($_GET['dirname']);}
-   if (isset($_GET['hashstr'])){$hashstr = rawurldecode($_GET['hashstr']);}
+   if (isset($_GET['input'])){$input = $_GET['input'];}
+   if (isset($_GET['dirname'])){$dirname = $_GET['dirname'];}
+   if (isset($_GET['hashstr'])){$hashstr = $_GET['hashstr'];}
    #print $input."<br>";
    #print $dirname."<br>";
    #print $hashstr."<br>";

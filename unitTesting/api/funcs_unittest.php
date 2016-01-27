8239ab77e16d57c62ab39a38407586fe46f23bb5
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2016-01-27 09:25:03 -0500

    api

--- unitTesting/api/funcs_unittest.php
+++ unitTesting/api/funcs_unittest.php
@@ -20,7 +20,24 @@ class funcs_unittest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($funcs->tool_path,'/usr/local/share/dolphin_tools/src');
 		$this->assertEquals($funcs->remotehost,'N');
 		$this->assertEquals($funcs->jobstatus,'N');
-		$this->assertEquals($funcs->config,'Localhost');
+		$this->assertEquals($funcs->config,'Docker');
+		$this->assertEquals($funcs->python,'python');
+		$this->assertEquals($funcs->scheduler,'N');
+		ob_end_clean();
+	}
+	
+	public function testGetINI(){
+		ob_start();
+		$funcs  = new funcs();
+		$funcs = $funcs->getINI();
+		$this->assertEquals($funcs->dbhost,'localhost');
+		$this->assertEquals($funcs->db,'biocore');
+		$this->assertEquals($funcs->dbuser,'bioinfo');
+		$this->assertEquals($funcs->dbpass,'bioinfo2013');
+		$this->assertEquals($funcs->tool_path,'/usr/local/share/dolphin_tools/src');
+		$this->assertEquals($funcs->remotehost,'N');
+		$this->assertEquals($funcs->jobstatus,'N');
+		$this->assertEquals($funcs->config,'Docker');
 		$this->assertEquals($funcs->python,'python');
 		$this->assertEquals($funcs->scheduler,'N');
 		ob_end_clean();

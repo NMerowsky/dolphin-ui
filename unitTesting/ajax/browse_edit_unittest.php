5ae6bd743deba990dc167867f3ae74a2f04a25d7
Rhaknam <alper.kucukural@umassmed.edu>
2016-01-26 19:42:22 -0500

    browse_edit

--- unitTesting/ajax/browse_edit_unittest.php
+++ unitTesting/ajax/browse_edit_unittest.php
@@ -16,8 +16,45 @@ class browse_edit_unittest extends PHPUnit_Framework_TestCase
 		include("browse_edit.php");
 		$this->assertEquals(json_decode($data),'1');
 		ob_end_clean();
-		
     }
+	
+	public function testCheckPerms(){
+		ob_start();
+		$_GET['p'] = 'checkPerms';
+        $_GET['id'] = '1';
+        $_GET['uid'] = '1';
+        $_GET['table'] = 'ngs_samples';
+		include("browse_edit.php");
+		$this->assertEquals(json_decode($data),'1');
+		ob_end_clean();
+	}
+	
+	public function testGetDropdownValues(){
+		ob_start();
+		$_GET['p'] = 'getDropdownValues';
+        $_GET['type'] = 'organism';
+		include("browse_edit.php");
+		$this->assertEquals(json_decode($data)[0]->organism,'human');
+		ob_end_clean();
+	}
+	
+	public function testGetExperimentPermissions(){
+		ob_start();
+		$_GET['p'] = 'getExperimentPermissions';
+        $_GET['experiments'] = '1';
+		include("browse_edit.php");
+		$this->assertEquals(json_decode($data)[0]->id,'1');
+		ob_end_clean();
+	}
+	
+	public function testGetLanePermissions(){
+		ob_start();
+		$_GET['p'] = 'getLanePermissions';
+        $_GET['lane'] = '1';
+		include("browse_edit.php");
+		$this->assertEquals(json_decode($data)[0]->id,'1');
+		ob_end_clean();
+	}
 }
 
 ?>

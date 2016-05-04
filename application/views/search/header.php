9feabccbd9a9567c731b3a0b7067fcf5bcf3f187
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-05-04 13:48:25 +0000

    switching branches

--- application/views/search/header.php
+++ application/views/search/header.php
@@ -65,6 +65,16 @@
 				</div>
 			</nav>
 		</header>
+		<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false">
+			<div class="modal-header">
+				<h1>Processing...</h1>
+			</div>
+			<div class="modal-body">
+				<div class="progress progress-striped active">
+					<div class="bar" style="width: 100%;"></div>
+				</div>
+			</div>
+		</div>
 		<div class="wrapper row-offcanvas row-offcanvas-left">
 			<!-- Left side column. contains the logo and sidebar -->
 			<aside class="left-side sidebar-offcanvas">
--- public/js/dolphin/js_to_html_funcs.js
+++ public/js/dolphin/js_to_html_funcs.js
@@ -58,3 +58,23 @@ function mergeTidy(headDiv, size, elementList){
 	}
 	return headDiv;
 }
+
+function addLoadingModal(){
+	
+}
+
+function toggleLoadingModal(){
+	var myApp;
+	myApp = myApp || (function () {
+		var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h1>Processing...</h1></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
+		return {
+			showPleaseWait: function() {
+				pleaseWaitDiv.modal();
+			},
+			hidePleaseWait: function () {
+				pleaseWaitDiv.modal('hide');
+			},
+	
+		};
+	})();
+}

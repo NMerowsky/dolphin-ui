5806053724a741a3a2c8058ab538a93e4dc7cd76
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-07-26 17:33:23 +0000

    test

--- unitTesting/travis_test.py
+++ unitTesting/travis_test.py
@@ -12,17 +12,16 @@ import time
 
 def main():
     url = "http://localhost:8080/public/api/service.php"
+    print url
     data = "wkey=&runparamsid=4&func=updateRunParams"
+    print data
     opener = urllib2.build_opener(urllib2.HTTPHandler())
+    print opener
     mesg = opener.open(url, data=data).read()
+    print mesg
     ret=str(json.loads(mesg))
     ret = re.sub (r'u\'', '\'', ret)
     ret = re.sub (r'\'', '\"', ret)
-    
-    print url
-    print data
-    print opener
-    print mesg
     print ret
 
 main()

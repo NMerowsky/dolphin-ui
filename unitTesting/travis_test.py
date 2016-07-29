d1f7a5497b70863410174791561d383a35c99c80
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-07-29 18:53:48 +0000

    test

--- unitTesting/travis_test.py
+++ unitTesting/travis_test.py
@@ -11,17 +11,20 @@ import sys
 import time
 
 def main():
-    url = "http://localhost/dolphin/public/api/service.php"
-    print url
-    data = "wkey=&sample_id=7&func=getFastqFileId"
-    print data
-    opener = urllib2.build_opener(urllib2.HTTPHandler())
-    print opener
-    mesg = opener.open(url, data=data).read()
-    print mesg
-    ret=str(json.loads(mesg))
-    ret = re.sub (r'u\'', '\'', ret)
-    ret = re.sub (r'\'', '\"', ret)
-    print ret
+    try:
+        url = "http://localhost/dolphin/public/api/service.php"
+        print url
+        data = "wkey=&sample_id=7&func=getFastqFileId"
+        print data
+        opener = urllib2.build_opener(urllib2.HTTPHandler())
+        print opener
+        mesg = opener.open(url, data=data).read()
+        print mesg
+        ret=str(json.loads(mesg))
+        ret = re.sub (r'u\'', '\'', ret)
+        ret = re.sub (r'\'', '\"', ret)
+        print ret
+    except Exception, ex:
+        print ex
 
 main()

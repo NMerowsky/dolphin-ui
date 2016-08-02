4f5ae256476fe6604ee79f5de57113b086632ac6
Rhaknam <alper.kucukural@umassmed.edu>
2016-08-02 09:41:10 -0400

    travis user

---
+++ db/2016_08_02_travisuser.sql
@@ -0,0 +1,4 @@
+INSERT INTO users
+(`username`,`clusteruser`,`role`,`name`.`email`,`email_toggle`,`institute`,`lab`,`owner`,`group_id`,`perms`)
+VALUES
+('travis','travis','travis','travis user','travis',0,'travis','travis',1,1,15);

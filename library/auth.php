7591948871196c181e01e6166ae19175800411e6
Rhaknam <alper.kucukural@umassmed.edu>
2016-08-15 08:51:53 -0400

    test

--- library/auth.php
+++ library/auth.php
@@ -3,7 +3,7 @@ if (!isset($_SESSION) || !is_array($_SESSION)) session_start();
 
 if (preg_match("/home/travis/build/", $http_host) )
 {
-  $param_section = "Docker";
+  $param_section = "Travis";
   $_SESSION['user'] = 'travis';
 }
 

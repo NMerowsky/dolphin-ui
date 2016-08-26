232d8bb21c2073dfeb5de5a224a695f0d1c4b8ab
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-08-26 17:09:03 +0000

    test

--- db/travis.sql
+++ db/travis.sql
@@ -6,3 +6,8 @@ VALUES
 INSERT INTO user_group (`u_id`,`g_id`,`owner_id`,`group_id`,`perms`,`date_created`,`date_modified`,`last_modified_user`)
 VALUES
 (209,1,1,1,15,NOW(),NOW(),1);
+
+UPDATE ngs_runparams SET owner_id = 209 where id = 4;
+UPDATE ngs_runlist SET owner_id = 209 where id IN (19,20);
+
+DELETE FROM amazon_credentials WHERE id = 1;

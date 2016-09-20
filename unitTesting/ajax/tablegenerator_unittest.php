6427f116cd4e06dc1df26d6c50a7d63e8c72e40b
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-09-20 18:23:21 +0000

    php unittest

--- unitTesting/ajax/tablegenerator_unittest.php
+++ unitTesting/ajax/tablegenerator_unittest.php
@@ -25,7 +25,7 @@ class tablegenerator_unittest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(json_decode($data)[0]->sample_id,'1');
 		$this->assertEquals(json_decode($data)[0]->run_id,'1');
 		$this->assertEquals(json_decode($data)[0]->run_name,'barcode test');
-		$this->assertEquals(json_decode($data)[0]->wkey,'J98Oe0bSZ18fBx9pPuDnsD8ITRVPGV');
+		$this->assertEquals(json_decode($data)[0]->wkey,'3pl8cmzYJ4ezgX2a9RevZxHmihpOA');
 		ob_end_clean();
 	}
 	
@@ -36,7 +36,7 @@ class tablegenerator_unittest extends PHPUnit_Framework_TestCase
 		$_GET['wkey'] = '3pl8cmzYJ4ezgX2a9RevZxHmihpOA';
 		include('tablegenerator.php');
 		$this->assertEquals(json_decode($data)[0]->file,'rsem/genes_expression_tpm.tsv');
-		$this->assertEquals(json_decode($data)[0]->json_parameters,'{"genomebuild":"mousetest,mm10","spaired":"paired","resume":"no","fastqc":"yes","barcodes":"distance,1:format,5 end read 1","adapter":"none","quality":"none","trim":"none","split":"none","commonind":"rRNA,miRNA,tRNA","pipeline":["RNASeqRSEM:--bowtie-e 70 --bowtie-chunkmbs 100:no:no","DESeq:control_rep1,control_rep2,control_rep3,exper_rep1,exper_rep2,exper_rep3:Cond1,Cond1,Cond1,Cond2,Cond2,Cond2:parametric:Yes:0.01:2"]}');
+		$this->assertEquals(json_decode($data)[0]->json_parameters,'{"genomebuild":"mousetest,mm10","spaired":"paired","resume":"resume","fastqc":"yes","barcodes":"distance,1:format,5 end read 1","adapter":"none","quality":"none","trim":"none","split":"none","commonind":"rRNA,miRNA,tRNA","pipeline":["RNASeqRSEM:--bowtie-e 70 --bowtie-chunkmbs 100:no:no"]}');
 		ob_end_clean();
 	}
 	

189f0e60cb3b308d58d4e240e05a87211ecd2fb5
Nick Merowsky <nmerowsky@gmail.com>
2016-11-18 13:02:00 -0500

    testing

--- public/api/funcs.php
+++ public/api/funcs.php
@@ -1043,14 +1043,18 @@ class funcs
         {
            $command=str_replace("\\\"", "\\\\\"", $command);
         }
-        $com = $this->python . " " . $this->tool_path . "/runService.py -f ".$this->config." -u " . $this->username .
-                " -o $backup_dir -k ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ -c \"$command\" -n stepMD5Sum_".explode(",",$file_name)[0]." -s stepMD5Sum_".explode(",",$file_name)[0];
-        $com = $this->getCMDs($com);
-        $retval = $this->sysback($com);
-        if (preg_match('/Error/', $retval)) {
-            return "ERROR: $retval : $com";
+        if($this->config != 'Travis'){
+            $com = $this->python . " " . $this->tool_path . "/runService.py -f ".$this->config." -u " . $this->username .
+                    " -o $backup_dir -k ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ -c \"$command\" -n stepMD5Sum_".explode(",",$file_name)[0]." -s stepMD5Sum_".explode(",",$file_name)[0];
+            $com = $this->getCMDs($com);
+            $retval = $this->sysback($com);
+            if (preg_match('/Error/', $retval)) {
+                return "ERROR: $retval : $com";
+            }
+            return "RUNNING: $retval : $com";
+        }else{
+            return "ERROR: cannot run on Travis";
         }
-        return "RUNNING: $retval : $com";
       }
       
       function dbMd5sumUpdate($params){

8670afaf2c2250e36124737a88602749c7ddcd85
Nick Merowsky <nmerowsky@gmail.com>
2016-11-18 13:35:50 -0500

    testing

--- public/api/funcs.php
+++ public/api/funcs.php
@@ -1044,7 +1044,9 @@ class funcs
             $command=str_replace("\\\"", "\\\\\"", $command);
             $com = $this->python . " " . $this->tool_path . "/runService.py -f ".$this->config." -u " . $this->username .
                     " -o $backup_dir -k ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ -c \"$command\" -n stepMD5Sum_".explode(",",$file_name)[0]." -s stepMD5Sum_".explode(",",$file_name)[0];
+            /*
             $com = $this->getCMDs($com);
+            */
             $retval = $this->sysback($com);
             if (preg_match('/Error/', $retval)) {
                 return "ERROR: $retval : $com";

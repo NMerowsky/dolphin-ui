4cb583805968247dc1b24f37625bdcfbebbe5269
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-11-18 16:50:42 +0000

    testing

--- public/api/funcs.php
+++ public/api/funcs.php
@@ -1043,8 +1043,6 @@ class funcs
         {
            $command=str_replace("\\\"", "\\\\\"", $command);
         }
-        return $command;
-        /*
         $com = $this->python . " " . $this->tool_path . "/runService.py -f ".$this->config." -u " . $this->username .
                 " -o $backup_dir -k ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ -c \"$command\" -n stepMD5Sum_".explode(",",$file_name)[0]." -s stepMD5Sum_".explode(",",$file_name)[0];
         $com = $this->getCMDs($com);
@@ -1053,7 +1051,6 @@ class funcs
             return "ERROR: $retval : $com";
         }
         return "RUNNING: $retval : $com";
-        */
       }
       
       function dbMd5sumUpdate($params){
